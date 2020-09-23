a1d96e9f5f34a5dc16c20afa74cfd22bef529a79
Warren Niu <warrenniu@gmail.com>
2020-09-23 19:08:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,6 +32,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     valid_calls.include?(element)
-
   end
 end
